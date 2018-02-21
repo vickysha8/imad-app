f916dcd258c6b093d30d90945b617f3496eca69c
vickysha8 <36630928+vickysha8@users.noreply.github.com>
2018-02-21 10:51:19 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,69 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    
+    title:'Article One | Vignesh sha',
+    heading: 'Article one',
+    date: 'Feb 21 2017',
+    content:`
+    
+                <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+            
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>
+            
+            <p>
+                This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article.
+            </p>    
+    
+    `
+    
+};
+
+Var htmltemplate= `
+
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+
+<link href="/ui/style.css" rel="stylesheet" />
+
+    </head>
+    
+
+    <body>
+        
+        <div class="container">
+            <Div>
+                
+                <a href="/">Home</a>
+            </Div>
+            <hr/>
+            <h3>
+               ${heading}
+            </h3>
+            <div>
+                ${date}
+            </div>
+            <div>
+            ${content}           
+            </div>
+        </div>
+    </body>
+    
+</html>
+`;
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/article-one',function(req,res){
     
