31006a0da45f64c51dcfecf5e355c77c2817dbb7
vickysha8 <36630928+vickysha8@users.noreply.github.com>
2018-02-21 09:12:02 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/Article-one',function(req,res){
+    
+    res.send('Article one requested and will be servered here');
+    });
+    
+app.get('/Article-two',function(req,res){
+    
+    res.send('Article two requested and will be servered here');
+    });
+    
+app.get('/Article-three',function(req,res){
+    
+    res.send('Article three requested and will be servered here');
+    });    
+    
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
