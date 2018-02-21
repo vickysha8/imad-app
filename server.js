dcfcdb1b2f4a0e86c5e328ed076158808287a36d
vickysha8 <36630928+vickysha8@users.noreply.github.com>
2018-02-21 09:40:33 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui','article-one.html'));
     });
     
-app.get('/article-one',function(req,res){
+app.get('/article-two',function(req,res){
     
     res.sendFile(path.join(__dirname, 'ui','article-two.html'));
     });
     
     
-app.get('/article-one',function(req,res){
+app.get('/article-three',function(req,res){
     
     res.sendFile(path.join(__dirname, 'ui','article-three.html'));
     });
