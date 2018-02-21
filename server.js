f8662480dfcd173a4c51ba729ed5bbd8b6a94e9b
vickysha8 <36630928+vickysha8@users.noreply.github.com>
2018-02-21 09:26:49 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/Article-one',function(req,res){
     
-    res.send('Article one requested and will be servered here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
     });
     
 app.get('/Article-two',function(req,res){
